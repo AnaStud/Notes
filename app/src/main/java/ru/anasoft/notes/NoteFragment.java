6304c424ef171f296f771a8eef2b12912ff9bb84
Anna <anasoft@yandex.ru>
2021-11-22 22:01:49 +0500

    4. Создала фрагмент для вывода данных

---
+++ app/src/main/java/ru/anasoft/notes/NoteFragment.java
@@ -0,0 +1,42 @@
+package ru.anasoft.notes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NoteFragment extends Fragment {
+
+    private static final String ARG_INDEX = "index";
+
+    private int index;
+
+    public NoteFragment() {
+        // Required empty public constructor
+    }
+
+     public static NoteFragment newInstance(int index) {
+        NoteFragment fragment = new NoteFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_INDEX, index);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_INDEX);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note, container, false);
+    }
+}
